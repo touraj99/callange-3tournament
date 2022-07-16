81d2bc7d9800f68cd52d00a785594e3557d2b083
touraj99 <touraj.tvk@gmail.com>
2022-07-16 19:00:36 +0430

    create superadmin and admin dashboard

--- challange/admin.py
+++ challange/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Member, UserAnswer, Question, Tournament, ResultMember
+
+admin.site.register(Member)
+admin.site.register(UserAnswer)
+admin.site.register(Question)
+admin.site.register(Tournament)
+admin.site.register(ResultMember)
